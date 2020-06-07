599d82ff0ff68fd6e08643880249d0f3bfd5b53a
RomanenkoAndrew <romanenkoandrey95@mail.ru>
2020-06-07 19:15:00 +0300

    fix: map

--- fancy-weather/src/scripts/App/helpers/getMap.js
+++ fancy-weather/src/scripts/App/helpers/getMap.js
@@ -3,12 +3,13 @@ import DATA from '../constants'
 
 const getMap = (latitude, longitude) => {
   mapboxgl.accessToken = DATA.MAP_TOKEN
-  return new mapboxgl.Map({
+  const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [longitude, latitude],
     zoom: 12
   })
+  new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map)
 }
 
 export default getMap
